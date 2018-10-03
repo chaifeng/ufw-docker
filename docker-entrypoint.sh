1c87b364f406f9f376c2ac23bfefdf168fe75eda
Chai Feng <chaifeng@chaifeng.com>
2018-10-03 09:15:21 +0800

    find all service ufw-docker-agent labels

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,13 @@ ufw_docker_agent_image=192.168.56.120:5000/ufw-docker-agent
 
 case "$1" in
     start)
+        docker service inspect "$ufw_docker_agent" \
+               --format '{{range $k,$v:=.Spec.Labels}}{{$k}} {{$v}}{{"\n"}}{{end}}' |
+            while read label port; do
+                [[ -z "$label" ]] && continue
+                name="${label#ufw.public.}"
+                echo "${name}=$port"
+            done
         sleep 60; exit 1
         ;;
     delete|allow)

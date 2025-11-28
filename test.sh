7dd160f9eaadf76fb902ffb3b435e17748022087
Chai Feng <chaifeng@chaifeng.com>
2025-11-28 11:10:42 +0800

    Add Bash version check (>= 4.3) to test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -uo pipefail
 
+if ((BASH_VERSINFO[0] < 4)) || ((BASH_VERSINFO[0] == 4 && BASH_VERSINFO[1] < 3)); then
+    echo "Error: Bash version must be >= 4.3"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Please install latest bash via brew: brew install bash"
+    fi
+    exit 1
+fi
+
 function out() {
     printf "\n\e[1;37;497;m%s\e[0;m\n" "$@"
 } >&2
